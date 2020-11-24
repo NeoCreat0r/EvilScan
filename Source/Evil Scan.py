a9cd904fb09398b3a5761fe1af635eedc4ce2306
NeoCreat0r <74102091+NeoCreat0r@users.noreply.github.com>
2020-11-24 10:43:15 +0000

    Сокращение строк кода
    Получил совет и отредактированный код моей программы (спасибо за помощь), решил перезалить и сделать более менее адекватный исходник

--- Source/Evil Scan.py
+++ Source/Evil Scan.py
@@ -24,8 +24,11 @@ print(f'''{Fore.LIGHTRED_EX}
 print(f"{Fore.LIGHTCYAN_EX} [#=>] Coded by NeoCreat0r")
 
 start = datetime.now()
-ports = [20, 21, 22, 23, 25, 42, 43, 53, 67, 69, 80, 115, 123, 137, 138, 139, 143, 161, 179, 443, 445, 514, 515, 993,
-         995, 1080, 1194, 1433, 1723, 3128, 3268, 3306, 3389, 5432, 5060, 5900, 8080, 10000, 20000]
+ports = {20: "FTP-DATA", 21: "FTP", 22: "SSH", 23: "Telnet", 25: "SMTP", 43: "WHOIS", 53: "DNS", 80: "http",
+                  115: "SFTP", 123: "NTP", 143: "IMAP", 161: "SNMP", 179: "BGP", 443: "HTTPS", 445: "MICROSOFT-DS",
+                  514: "SYSLOG", 515: "PRINTER", 993: "IMAPS", 995: "POP3S", 1080: "SOCKS", 1194: "OpenVPN",
+                  1433: "SQL Server", 1723: "PPTP", 3128: "HTTP", 3268: "LDAP", 3306: "MySQL", 3389: "RDP",
+                  5432: "PostgreSQL", 5060: "SIP", 5900: "VNC", 8080: "Tomcat", 10000: "Webmin"}
 
 print(f"{Fore.LIGHTYELLOW_EX}")
 ip = input("Site Name / IP Address: ")
@@ -34,81 +37,15 @@ print(f"{Fore.LIGHTRED_EX}<===============>")
 print(f"{Fore.WHITE}Loading Ports...")
 print(f"{Fore.LIGHTGREEN_EX}##################")
 for port in ports:
-    y = socket.socket()
-    y.settimeout(1)
+    s = socket.socket()
+    s.settimeout(1)
     try:
-        y.connect((ip, port))
+        s.connect((ip, port))
     except socket.error:
         pass
     else:
-        y.close()
-
-        if port == 20:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/FTP-DATA")
-        elif port == 21:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/FTP")
-        elif port == 22:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SSH")
-        elif port == 23:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/Telnet")
-        elif port == 25:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SMTP")
-        elif port == 43:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/WHOIS")
-        elif port == 53:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/DNS")
-        elif port == 80:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/http")
-        elif port == 115:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SFTP")
-        elif port == 123:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/NTP")
-        elif port == 143:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/IMAP")
-        elif port == 161:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SNMP")
-        elif port == 179:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/BGP")
-        elif port == 443:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/HTTPS")
-        elif port == 445:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/MICROSOFT-DS")
-        elif port == 514:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SYSLOG")
-        elif port == 515:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/PRINTER")
-        elif port == 993:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/IMAPS")
-        elif port == 995:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/POP3S")
-        elif port == 1080:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SOCKS")
-        elif port == 1194:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/OpenVPN")
-        elif port == 1433:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SQL Server")
-        elif port == 1723:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/PPTP")
-        elif port == 3128:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/HTTP")
-        elif port == 3268:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/LDAP")
-        elif port == 3306:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/MySQL")
-        elif port == 3389:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/RDP")
-        elif port == 5432:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/PostgreSQL")
-        elif port == 5060:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/SIP")
-        elif port == 5900:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/VNC")
-        elif port == 8080:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/Tomcat")
-        elif port == 10000:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/Webmin")
-        else:
-            print(ip + ":" + str(port) + f" {Fore.LIGHTGREEN_EX}is open/unknown")
+        print(f"{socket.gethostbyname(ip)}:{str(port)} {Fore.LIGHTGREEN_EX}is open/{ports[port]}")
+        s.close()
 
 print(f"{Fore.LIGHTGREEN_EX}##################")
 ends = datetime.now()
